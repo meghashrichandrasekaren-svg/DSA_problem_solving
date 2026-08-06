802b63ec5ef689ae791b5fcb856d0711fc166647
Meghashri <meghashrichandrasekaren@gmail.com>
2026-08-06 05:41:22 +0530

    Add missingNumber function to find missing number
    Implement a solution to find the missing number in an array.

---
+++ LeetCode/missing-number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<=n;i++){
+            boolean flag=false;
+            for(int j=0;j<n;j++){
+                if(nums[j]==i){
+                    flag=true;
+                    break;
+                }
+                
+        
+                
+            }
+           if (!flag){
+                    return i;
+                }
+           
+        }
+        return 0;
+        
+    }
+}
