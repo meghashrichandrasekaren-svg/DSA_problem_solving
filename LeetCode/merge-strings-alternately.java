2b2a1b75168e31f5d3ae18f1f2a87ef716121d62
Meghashri <meghashrichandrasekaren@gmail.com>
2026-08-29 20:47:35 +0530

    Create merge-strings-alternately.java

---
+++ LeetCode/merge-strings-alternately.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        String res="";
+        int i=0;
+    while(i<word1.length()&&i<word2.length()){
+         res+=word1.charAt(i);
+         res+=word2.charAt(i);
+         i++;
+    }
+    while(i<word1.length()){
+res+=word1.charAt(i);
+i++;
+    }
+    while(i<word2.length()){
+res+=word2.charAt(i);
+i++;
+    }
+
+    return res;
+    }
+    
+}
